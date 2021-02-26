ed21bf26b3cd5f3d3083c0529b1abff686f80751
EC2 Default User <ec2-user@ip-172-31-26-195.ap-northeast-2.compute.internal>
2021-02-26 01:29:41 +0000

    data remote

--- main.tf
+++ main.tf
@@ -9,8 +9,27 @@ data "aws_security_group" "default" {
   vpc_id = local.vpc_id
 }
 
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "terraexam"
+    workspaces = {
+      name = "terraexam-aws-vpc"
+    }
+  }
+}
+
+# resource "aws_instance" "redis_server" {
+#   # Terraform 0.12 syntax: use the "outputs.<OUTPUT NAME>" attribute
+#   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
+
+#   # Terraform 0.11 syntax: use the "<OUTPUT NAME>" attribute
+#   subnet_id = "${data.terraform_remote_state.vpc.subnet_id}"
+# }
+
+
 locals {
-  vpc_id = "vpc-04bc8955784f0fa6d"
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id //"vpc-04bc8955784f0fa6d"
   vpc_cidr_block = "10.0.0.0/16"
   bastion_security_group_ids = ["sg-0b28759a17906d1e8"]
   public_subnet_ids = ["subnet-0d731cbf3711d6ba5","subnet-092b6128a970b5666"]
