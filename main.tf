45911d416b0446aa0dc362b8a62471248818cbe2
EC2 Default User <ec2-user@ip-172-31-26-195.ap-northeast-2.compute.internal>
2021-02-26 01:49:07 +0000

    modi to data

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ data "aws_security_group" "default" {
 locals {
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id //"vpc-04bc8955784f0fa6d"
   vpc_cidr_block = data.terraform_remote_state.vpc.outputs.vpc_cidr_block //"10.0.0.0/16"
-  bastion_security_group_ids = data.terraform_remote_state.sg.outputs.bastion_security_group_ids  //["sg-0b28759a17906d1e8"]
+  bastion_security_group_ids = data.terraform_remote_state.sg.outputs.bastion_security_group_id  //["sg-0b28759a17906d1e8"]
   public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnets  //["subnet-0d731cbf3711d6ba5","subnet-092b6128a970b5666"]
   private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets  //["subnet-05f4aa0ef2c3d01eb","subnet-0324151bd7d5f1577"]
   database_subnet_ids = data.terraform_remote_state.vpc.outputs.database_subnets  //["subnet-042c5dad0a4d8d1a0","subnet-0cc660ffb3b50fcf2"]
