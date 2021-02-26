853a534bfd70fd72c497d5aeae079ac0958de4c3
EC2 Default User <ec2-user@ip-172-31-26-195.ap-northeast-2.compute.internal>
2021-02-26 01:48:32 +0000

    modi to data

--- main.tf
+++ main.tf
@@ -3,12 +3,7 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_security_group" "default" {
-  name = "default"
-  #   vpc_id = module.vpc.vpc_id
-  vpc_id = local.vpc_id
-}
-
+# Workspace - vpc
 data "terraform_remote_state" "vpc" {
   backend = "remote"
   config = {
@@ -19,6 +14,19 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+# Workspace - security group
+data "terraform_remote_state" "sg" {
+  backend = "remote"
+  config = {
+    organization = "terraexam"
+    workspaces = {
+      name = "terraexam-aws-sg"
+    }
+  }
+}
+
+### Usage ###
+#
 # resource "aws_instance" "redis_server" {
 #   # Terraform 0.12 syntax: use the "outputs.<OUTPUT NAME>" attribute
 #   subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
@@ -28,13 +36,21 @@ data "terraform_remote_state" "vpc" {
 # }
 
 
+# Resource - deafult security group
+data "aws_security_group" "default" {
+  name = "default"
+  #   vpc_id = module.vpc.vpc_id
+  vpc_id = local.vpc_id
+}
+
+
 locals {
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id //"vpc-04bc8955784f0fa6d"
-  vpc_cidr_block = "10.0.0.0/16"
-  bastion_security_group_ids = ["sg-0b28759a17906d1e8"]
-  public_subnet_ids = ["subnet-0d731cbf3711d6ba5","subnet-092b6128a970b5666"]
-  private_subnet_ids = ["subnet-05f4aa0ef2c3d01eb","subnet-0324151bd7d5f1577"]
-  database_subnet_ids = ["subnet-042c5dad0a4d8d1a0","subnet-0cc660ffb3b50fcf2"]
+  vpc_cidr_block = data.terraform_remote_state.vpc.outputs.vpc_cidr_block //"10.0.0.0/16"
+  bastion_security_group_ids = data.terraform_remote_state.sg.outputs.bastion_security_group_ids  //["sg-0b28759a17906d1e8"]
+  public_subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnets  //["subnet-0d731cbf3711d6ba5","subnet-092b6128a970b5666"]
+  private_subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets  //["subnet-05f4aa0ef2c3d01eb","subnet-0324151bd7d5f1577"]
+  database_subnet_ids = data.terraform_remote_state.vpc.outputs.database_subnets  //["subnet-042c5dad0a4d8d1a0","subnet-0cc660ffb3b50fcf2"]
 }
 
 module "bastion" {
