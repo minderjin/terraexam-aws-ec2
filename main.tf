4c88ef4c517f09f6264b28d619854f3cc1a5cc55
EC2 Default User <ec2-user@ip-172-31-26-195.ap-northeast-2.compute.internal>
2021-02-26 04:56:48 +0000

    add was inst

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ module "bastion" {
   instance_count = 1
 
   name                        = "${var.name}-bastion"
-  ami                         = data.aws_ami.amazon_linux.id  //"ami-09c5e030f74651050" // Amazon Linux 2 
+  ami                         = data.aws_ami.amazon_linux.id  //"ami-09c5e030f74651050" //Amazon Linux 2 
   instance_type               = "t2.micro"
   subnet_id                   = local.public_subnet_ids[0]
   vpc_security_group_ids      = local.bastion_security_group_ids
@@ -114,6 +114,48 @@ module "bastion" {
   cpu_credits                 = "unlimited"
 
   user_data_base64 = base64encode(local.user_data)
+
+  disable_api_termination = false
+
+  root_block_device = [
+    {
+      volume_type           = "gp3"
+      volume_size           = 8
+      delete_on_termination = true
+    },
+  ]
+
+  # ebs_block_device = [
+  #   {
+  #     device_name = "/dev/sdf"
+  #     volume_type = "gp3"
+  #     volume_size = 10
+  #     encrypted   = true
+  #     kms_key_id  = aws_kms_key.this.arn
+  #   }
+  # ]
+
+  tags = var.tags
+}
+
+
+module "was" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 2.0"
+
+  instance_count = 1
+
+  name                        = "${var.name}-was"
+  ami                         = data.aws_ami.amazon_linux.id  //"ami-09c5e030f74651050" //Amazon Linux 2 
+  instance_type               = "t3.micro"
+  subnet_id                   = local.private_subnet_ids[0]
+  vpc_security_group_ids      = local.was_security_group_ids
+  associate_public_ip_address = false
+  key_name                    = "oregon-key"
+  monitoring                  = false
+  cpu_credits                 = "unlimited"
+
+  user_data_base64 = base64encode(local.was_user_data)
 
   disable_api_termination = false
 
